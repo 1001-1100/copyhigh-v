1ae9348b940720ece636f527156f31d39adcf93b
1001-1100 <joshuacapulemanzano@gmail.com>
2020-05-19 20:46:02 +0800

    database

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn dlsuscheduler.wsgi --log-file -
+release: python manage.py makemigrations hello && python manage.py migrate hello --no-input && python manage.py makemigrations && python manage.py migrate --no-input
+web: gunicorn dlsuscheduler.wsgi
--- dlsuscheduler/settings.py
+++ dlsuscheduler/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
+import django_heroku
 
+IS_LOCAL = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,12 +79,17 @@ WSGI_APPLICATION = "dlsuscheduler.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+if(IS_LOCAL):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True) 
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
